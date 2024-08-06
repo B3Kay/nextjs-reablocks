9cdab89ece265f96c27acdb455b594902c22d9a3
B3Kay <benjaminjekarlsson@gmail.com>
2024-08-06 15:54:59 +0200

    Add reablocks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider, theme } from 'reablocks';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider theme={theme}>
+
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
